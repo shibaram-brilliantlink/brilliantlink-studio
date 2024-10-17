b4a6ac6d67d52812298610a38ff1a6bd74a1c33c
shibaram-brilliantlink <shibaram@brilliantlink.tech>
2024-10-17 16:24:36 +0530

    promote popup now can be closed.

--- src/components/PromotePopup.tsx
+++ src/components/PromotePopup.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function PromotePopup({
@@ -7,61 +8,80 @@ export default function PromotePopup({
   title: string;
   subtitle: string;
 }) {
+  const [toClose, setToClose] = useState(false);
   return (
-    <WrapperCard>
-      <div className="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-          <g
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            id="SVGRepo_tracerCarrier"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            {" "}
-            <path
-              stroke-linejoin="round"
-              stroke-linecap="round"
-              stroke-width="1.5"
-              stroke="#ffffff"
-              d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"
-            ></path>{" "}
-          </g>
-        </svg>
-      </div>
-      <div className="content">
-        <span className="title">{title || "Good news Mac users!"}</span>
-        <div className="desc">
-          {subtitle || "This software is now available for download."}
-        </div>
-        <div className="actions">
-          <div>
-            <a href="#" className="download">
-              Try it free
-            </a>
+    <>
+      {toClose == false ? (
+        <WrapperCard>
+          <div className="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+              <g
+                stroke-linejoin="round"
+                stroke-linecap="round"
+                id="SVGRepo_tracerCarrier"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="1.5"
+                  stroke="#ffffff"
+                  d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"
+                ></path>{" "}
+              </g>
+            </svg>
           </div>
-          <div>
-            <a href="#" className="notnow">
-              Not now
-            </a>
+          <div className="content">
+            <span className="title">{title || "Good news Mac users!"}</span>
+            <div className="desc">
+              {subtitle || "This software is now available for download."}
+            </div>
+            <div className="actions">
+              <div>
+                <a href="#" className="download">
+                  Try it free
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="notnow"
+                  onClick={() => setToClose(!toClose)}
+                >
+                  Not now
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <button type="button" className="close">
-        <svg
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </button>
-    </WrapperCard>
+          <button
+            type="button"
+            className="close"
+            onClick={() => setToClose(!toClose)}
+          >
+            <svg
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </WrapperCard>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 const WrapperCard = styled.div`
